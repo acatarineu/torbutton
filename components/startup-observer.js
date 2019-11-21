2a30e40df04d2f006fa80c4a0694086960466ad3
Alex Catarineu <acat@torproject.org>
2019-11-21 14:22:06 +0100

    Fix eslint errors

--- components/startup-observer.js
+++ components/startup-observer.js
@@ -13,7 +13,9 @@
  *************************************************************************/
 
 const { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-const { XPCOMUtils } = ChromeUtils.import("resource://gre/modules/XPCOMUtils.jsm");
+const { XPCOMUtils } = ChromeUtils.import(
+  "resource://gre/modules/XPCOMUtils.jsm"
+);
 
 XPCOMUtils.defineLazyModuleGetters(this, {
   FileUtils: "resource://gre/modules/FileUtils.jsm",
@@ -21,7 +23,10 @@ XPCOMUtils.defineLazyModuleGetters(this, {
   L10nRegistry: "resource://gre/modules/L10nRegistry.jsm",
 });
 
-let NoScriptControl = ChromeUtils.import("resource://torbutton/modules/noscript-control.js", {});
+let NoScriptControl = ChromeUtils.import(
+  "resource://torbutton/modules/noscript-control.js",
+  {}
+);
 
 // Module specific constants
 const kMODULE_NAME = "Startup";
@@ -45,154 +50,258 @@ function cleanupCookies() {
 }
 
 function StartupObserver() {
-    this.logger = Cc["@torproject.org/torbutton-logger;1"]
-                    .getService(Ci.nsISupports).wrappedJSObject;
-    this._prefs = Services.prefs;
-    this.logger.log(3, "Startup Observer created");
+  this.logger = Cc["@torproject.org/torbutton-logger;1"].getService(
+    Ci.nsISupports
+  ).wrappedJSObject;
+  this._prefs = Services.prefs;
+  this.logger.log(3, "Startup Observer created");
 
-    var env = Cc["@mozilla.org/process/environment;1"]
-                .getService(Ci.nsIEnvironment);
-    var prefName = "browser.startup.homepage";
-    if (env.exists("TOR_DEFAULT_HOMEPAGE")) {
-      // if the user has set this value in a previous installation, don't override it
-      if (!this._prefs.prefHasUserValue(prefName)) {
-        this._prefs.setCharPref(prefName, env.get("TOR_DEFAULT_HOMEPAGE"));
-      }
+  var env = Cc["@mozilla.org/process/environment;1"].getService(
+    Ci.nsIEnvironment
+  );
+  var prefName = "browser.startup.homepage";
+  if (env.exists("TOR_DEFAULT_HOMEPAGE")) {
+    // if the user has set this value in a previous installation, don't override it
+    if (!this._prefs.prefHasUserValue(prefName)) {
+      this._prefs.setCharPref(prefName, env.get("TOR_DEFAULT_HOMEPAGE"));
     }
+  }
 
-    try {
-      var test = this._prefs.getCharPref("torbrowser.version");
-      this.is_tbb = true;
-      this.logger.log(3, "This is a Tor Browser's XPCOM");
-    } catch(e) {
-      this.logger.log(3, "This is not a Tor Browser's XPCOM");
-    }
+  try {
+    this._prefs.getCharPref("torbrowser.version");
+    this.is_tbb = true;
+    this.logger.log(3, "This is a Tor Browser's XPCOM");
+  } catch (e) {
+    this.logger.log(3, "This is not a Tor Browser's XPCOM");
+  }
 
-    try {
-      // XXX: We're in a race with HTTPS-Everywhere to update our proxy settings
-      // before the initial SSL-Observatory test... If we lose the race, Firefox
-      // caches the old proxy settings for check.tp.o somehwere, and it never loads :(
-      this.setProxySettings();
-    } catch(e) {
-      this.logger.log(4, "Early proxy change failed. Will try again at profile load. Error: "+e);
-    }
-
-    cleanupCookies();
-
-    // Using all possible locales so that we do not have to change this list every time we support
-    // a new one.
-    const allLocales = [
-      "en-US", "ach", "af", "an", "ar", "ast", "az", "be", "bg", "bn", "br", "bs", "ca", "cak",
-      "crh", "cs", "cy", "da", "de", "dsb", "el", "en-CA", "en-GB", "eo", "es-AR", "es-CL",
-      "es-ES", "es-MX", "et", "eu", "fa", "ff", "fi", "fr", "fy-NL", "ga-IE", "gd", "gl", "gn",
-      "gu-IN", "he", "hi-IN", "hr", "hsb", "hu", "hy-AM", "ia", "id", "is", "it", "ja",
-      "ja-JP-mac", "ka", "kab", "kk", "km", "kn", "ko", "lij", "lo", "lt", "ltg", "lv", "mk", "mr",
-      "ms", "my", "nb-NO", "ne-NP", "nl", "nn-NO", "oc", "pa-IN", "pl", "pt-BR", "pt-PT", "rm",
-      "ro", "ru", "si", "sk", "sl", "son", "sq", "sr", "sv-SE", "ta", "te", "th", "tl", "tr",
-      "trs", "uk", "ur", "uz", "vi", "wo", "xh", "zh-CN", "zh-TW"
-    ];
-    let torSource = new FileSource(
-      "torbutton",
-      allLocales,
-      "resource://torbutton/locale/{locale}/",
-      true, // skip this FileSource locales when computing Services.locale.availableLocales
+  try {
+    // XXX: We're in a race with HTTPS-Everywhere to update our proxy settings
+    // before the initial SSL-Observatory test... If we lose the race, Firefox
+    // caches the old proxy settings for check.tp.o somehwere, and it never loads :(
+    this.setProxySettings();
+  } catch (e) {
+    this.logger.log(
+      4,
+      "Early proxy change failed. Will try again at profile load. Error: " + e
     );
-    L10nRegistry.registerSource(torSource);
+  }
+
+  cleanupCookies();
+
+  // Using all possible locales so that we do not have to change this list every time we support
+  // a new one.
+  const allLocales = [
+    "en-US",
+    "ach",
+    "af",
+    "an",
+    "ar",
+    "ast",
+    "az",
+    "be",
+    "bg",
+    "bn",
+    "br",
+    "bs",
+    "ca",
+    "cak",
+    "crh",
+    "cs",
+    "cy",
+    "da",
+    "de",
+    "dsb",
+    "el",
+    "en-CA",
+    "en-GB",
+    "eo",
+    "es-AR",
+    "es-CL",
+    "es-ES",
+    "es-MX",
+    "et",
+    "eu",
+    "fa",
+    "ff",
+    "fi",
+    "fr",
+    "fy-NL",
+    "ga-IE",
+    "gd",
+    "gl",
+    "gn",
+    "gu-IN",
+    "he",
+    "hi-IN",
+    "hr",
+    "hsb",
+    "hu",
+    "hy-AM",
+    "ia",
+    "id",
+    "is",
+    "it",
+    "ja",
+    "ja-JP-mac",
+    "ka",
+    "kab",
+    "kk",
+    "km",
+    "kn",
+    "ko",
+    "lij",
+    "lo",
+    "lt",
+    "ltg",
+    "lv",
+    "mk",
+    "mr",
+    "ms",
+    "my",
+    "nb-NO",
+    "ne-NP",
+    "nl",
+    "nn-NO",
+    "oc",
+    "pa-IN",
+    "pl",
+    "pt-BR",
+    "pt-PT",
+    "rm",
+    "ro",
+    "ru",
+    "si",
+    "sk",
+    "sl",
+    "son",
+    "sq",
+    "sr",
+    "sv-SE",
+    "ta",
+    "te",
+    "th",
+    "tl",
+    "tr",
+    "trs",
+    "uk",
+    "ur",
+    "uz",
+    "vi",
+    "wo",
+    "xh",
+    "zh-CN",
+    "zh-TW",
+  ];
+  let torSource = new FileSource(
+    "torbutton",
+    allLocales,
+    "resource://torbutton/locale/{locale}/",
+    true // skip this FileSource locales when computing Services.locale.availableLocales
+  );
+  L10nRegistry.registerSource(torSource);
 }
 
 StartupObserver.prototype = {
-    // Bug 6803: We need to get the env vars early due to
-    // some weird proxy caching code that showed up in FF15.
-    // Otherwise, homepage domain loads fail forever.
-    setProxySettings: function() {
-      if (!this.is_tbb)
-        return;
+  // Bug 6803: We need to get the env vars early due to
+  // some weird proxy caching code that showed up in FF15.
+  // Otherwise, homepage domain loads fail forever.
+  setProxySettings() {
+    if (!this.is_tbb) {
+      return;
+    }
 
-      // Bug 1506: Still want to get these env vars
-      let environ = Cc["@mozilla.org/process/environment;1"]
-                      .getService(Ci.nsIEnvironment);
-      if (environ.exists("TOR_TRANSPROXY")) {
-        this.logger.log(3, "Resetting Tor settings to transproxy");
-        this._prefs.setBoolPref("network.proxy.socks_remote_dns", false);
-        this._prefs.setIntPref("network.proxy.type", 0);
-        this._prefs.setIntPref("network.proxy.socks_port", 0);
-        this._prefs.setCharPref("network.proxy.socks", "");
-      } else {
-        // Try to retrieve SOCKS proxy settings from Tor Launcher.
-        let socksPortInfo;
-        try {
-          let tlps = Cc["@torproject.org/torlauncher-protocol-service;1"]
-                     .getService(Ci.nsISupports).wrappedJSObject;
-          socksPortInfo = tlps.TorGetSOCKSPortInfo();
-        } catch(e) {
-          this.logger.log(3, "tor launcher failed " + e);
-        }
+    // Bug 1506: Still want to get these env vars
+    let environ = Cc["@mozilla.org/process/environment;1"].getService(
+      Ci.nsIEnvironment
+    );
+    if (environ.exists("TOR_TRANSPROXY")) {
+      this.logger.log(3, "Resetting Tor settings to transproxy");
+      this._prefs.setBoolPref("network.proxy.socks_remote_dns", false);
+      this._prefs.setIntPref("network.proxy.type", 0);
+      this._prefs.setIntPref("network.proxy.socks_port", 0);
+      this._prefs.setCharPref("network.proxy.socks", "");
+    } else {
+      // Try to retrieve SOCKS proxy settings from Tor Launcher.
+      let socksPortInfo;
+      try {
+        let tlps = Cc[
+          "@torproject.org/torlauncher-protocol-service;1"
+        ].getService(Ci.nsISupports).wrappedJSObject;
+        socksPortInfo = tlps.TorGetSOCKSPortInfo();
+      } catch (e) {
+        this.logger.log(3, "tor launcher failed " + e);
+      }
 
-        // If Tor Launcher is not available, check environment variables.
-        if (!socksPortInfo) {
-          socksPortInfo = { ipcFile: undefined, host: undefined, port: 0 };
+      // If Tor Launcher is not available, check environment variables.
+      if (!socksPortInfo) {
+        socksPortInfo = { ipcFile: undefined, host: undefined, port: 0 };
 
-          let isWindows = Services.appinfo.OS === "WINNT";
-          if (!isWindows && environ.exists("TOR_SOCKS_IPC_PATH")) {
-            socksPortInfo.ipcFile = new FileUtils.File(
-                                           environ.get("TOR_SOCKS_IPC_PATH"));
-          }
-          else
-          {
-            if (environ.exists("TOR_SOCKS_HOST"))
-              socksPortInfo.host = environ.get("TOR_SOCKS_HOST");
-            if (environ.exists("TOR_SOCKS_PORT"))
-              socksPortInfo.port = parseInt(environ.get("TOR_SOCKS_PORT"));
-          }
-        }
-
-        // Adjust network.proxy prefs.
-        if (socksPortInfo.ipcFile) {
-          let fph = Services.io.getProtocolHandler("file")
-                               .QueryInterface(Ci.nsIFileProtocolHandler);
-          let fileURI = fph.newFileURI(socksPortInfo.ipcFile);
-          this.logger.log(3, "Reset socks to "+fileURI.spec);
-          this._prefs.setCharPref("network.proxy.socks", fileURI.spec);
-          this._prefs.setIntPref("network.proxy.socks_port", 0);
+        let isWindows = Services.appinfo.OS === "WINNT";
+        if (!isWindows && environ.exists("TOR_SOCKS_IPC_PATH")) {
+          socksPortInfo.ipcFile = new FileUtils.File(
+            environ.get("TOR_SOCKS_IPC_PATH")
+          );
         } else {
-          if (socksPortInfo.host) {
-            this._prefs.setCharPref("network.proxy.socks", socksPortInfo.host);
-            this.logger.log(3, "Reset socks host to "+socksPortInfo.host);
+          if (environ.exists("TOR_SOCKS_HOST")) {
+            socksPortInfo.host = environ.get("TOR_SOCKS_HOST");
           }
-          if (socksPortInfo.port) {
-            this._prefs.setIntPref("network.proxy.socks_port",
-                                   socksPortInfo.port);
-            this.logger.log(3, "Reset socks port to "+socksPortInfo.port);
+          if (environ.exists("TOR_SOCKS_PORT")) {
+            socksPortInfo.port = parseInt(environ.get("TOR_SOCKS_PORT"));
           }
-        }
-
-        if (socksPortInfo.ipcFile || socksPortInfo.host || socksPortInfo.port) {
-          this._prefs.setBoolPref("network.proxy.socks_remote_dns", true);
-          this._prefs.setIntPref("network.proxy.type", 1);
         }
       }
 
-      // Force prefs to be synced to disk
-      Services.prefs.savePrefFile(null);
-
-      this.logger.log(3, "Synced network settings to environment.");
-    },
-
-    observe: function(subject, topic, data) {
-      if(topic == "profile-after-change") {
-        // Bug 1506 P1: We listen to these prefs as signals for startup,
-        // but only for hackish reasons.
-        this._prefs.setBoolPref("extensions.torbutton.startup", true);
-
-	// We need to listen for NoScript before it starts.
-        NoScriptControl.initialize();
-
-        this.setProxySettings();
+      // Adjust network.proxy prefs.
+      if (socksPortInfo.ipcFile) {
+        let fph = Services.io
+          .getProtocolHandler("file")
+          .QueryInterface(Ci.nsIFileProtocolHandler);
+        let fileURI = fph.newFileURI(socksPortInfo.ipcFile);
+        this.logger.log(3, "Reset socks to " + fileURI.spec);
+        this._prefs.setCharPref("network.proxy.socks", fileURI.spec);
+        this._prefs.setIntPref("network.proxy.socks_port", 0);
+      } else {
+        if (socksPortInfo.host) {
+          this._prefs.setCharPref("network.proxy.socks", socksPortInfo.host);
+          this.logger.log(3, "Reset socks host to " + socksPortInfo.host);
+        }
+        if (socksPortInfo.port) {
+          this._prefs.setIntPref(
+            "network.proxy.socks_port",
+            socksPortInfo.port
+          );
+          this.logger.log(3, "Reset socks port to " + socksPortInfo.port);
+        }
       }
 
-      // In all cases, force prefs to be synced to disk
-      Services.prefs.savePrefFile(null);
-    },
+      if (socksPortInfo.ipcFile || socksPortInfo.host || socksPortInfo.port) {
+        this._prefs.setBoolPref("network.proxy.socks_remote_dns", true);
+        this._prefs.setIntPref("network.proxy.type", 1);
+      }
+    }
+
+    // Force prefs to be synced to disk
+    Services.prefs.savePrefFile(null);
+
+    this.logger.log(3, "Synced network settings to environment.");
+  },
+
+  observe(subject, topic, data) {
+    if (topic == "profile-after-change") {
+      // Bug 1506 P1: We listen to these prefs as signals for startup,
+      // but only for hackish reasons.
+      this._prefs.setBoolPref("extensions.torbutton.startup", true);
+
+      // We need to listen for NoScript before it starts.
+      NoScriptControl.initialize();
+
+      this.setProxySettings();
+    }
+
+    // In all cases, force prefs to be synced to disk
+    Services.prefs.savePrefFile(null);
+  },
 
   QueryInterface: ChromeUtils.generateQI([Ci.nsIClassInfo]),
 
@@ -202,7 +311,7 @@ StartupObserver.prototype = {
   contractID: kMODULE_CONTRACTID,
 
   // Hack to get us registered early to observe recovery
-  _xpcom_categories: [{category:"profile-after-change"}],
+  _xpcom_categories: [{ category: "profile-after-change" }],
 };
 
 var NSGetFactory = XPCOMUtils.generateNSGetFactory([StartupObserver]);
